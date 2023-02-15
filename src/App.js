de51a46978c81539fe8712815a22b649f561b2a5
Juan Ignacio Marchetto Lobianco <juanignaciomarchetto@gmail.com>
2023-02-15 08:43:03 -0300

    15/2/2023

---
+++ src/App.js
@@ -0,0 +1,18 @@
+let alumnos
+[
+  
+    id=0,namme="pepe",
+    year=2,
+    absenses=5,
+    average=7,
+  
+  ,
+  
+    id=0,
+    namme="pepe",
+    year="2",
+    absenses="5",
+    average="7"
+  
+
+]
